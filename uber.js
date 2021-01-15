6604c9424869d0ad86c511399720ddc18a3c31a9
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-15 18:07:00 -0300

    :sparkles: feat : uber challenge done!

---
+++ uber.js
@@ -0,0 +1,27 @@
+function descobrirMelhorMotorista(motoristas){
+    const formulaDoMelhorMotorista = (corridas,estrelas,distancia) => (corridas / (estrelas * distancia))
+
+    let maiorNota = 0
+    let motoristaComMaiorNota = {}
+
+    motoristas.forEach(motorista => {
+        const { corridas,estrelas,distancia } = motorista
+        motorista.nota = formulaDoMelhorMotorista(corridas,estrelas,distancia)
+        if(maiorNota < motorista.nota) {
+            maiorNota = motorista.nota 
+            motoristaComMaiorNota = motorista
+        }
+    })
+
+    return motoristaComMaiorNota
+}
+
+const motoristas = [
+    { distancia:3 ,estrelas:4.7,corridas: 3000 },
+    { distancia:5 ,estrelas:4.2,corridas: 2700 },
+    { distancia:4.5 ,estrelas:3.7,corridas: 2800 },
+    { distancia:7 ,estrelas:4.8,corridas:1900 },
+    { distancia:6 ,estrelas:4.4,corridas:4400 },
+]
+
+console.log( descobrirMelhorMotorista(motoristas))
